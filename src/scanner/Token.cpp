6a706ea480285ef300cd38bc375e984b548b34e4
BickfordA <aidan.bickford@gmail.com>
2015-01-28 17:15:57 -0700

    Fix compile errors

--- src/scanner/Token.cpp
+++ src/scanner/Token.cpp
@@ -6,8 +6,8 @@ Token::Token()
 {
 }
 
-Token::Token(string name, Lexeme lexeme, int lineNumber, int columnNumber)
-:_lineNumber(lineNumber), _columnNumber(columnNumber), _name(name), _lexeme(lexeme)
+Token::Token( Lexeme lexeme, int lineNumber, int columnNumber)
+:_lineNumber(lineNumber), _columnNumber(columnNumber), _lexeme(lexeme)
 {
 }
 
