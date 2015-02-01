8fb051508e41311c69e8732522fb789a9e3dfa88
BickfordA <aidan.bickford@gmail.com>
2015-02-01 12:14:33 -0700

    Updated type enums and descriptive strings

--- src/scanner/Lexeme.h
+++ src/scanner/Lexeme.h
@@ -9,23 +9,96 @@ class Lexeme {
     
 public:
 	//add more types as we go here 
-	enum Type{ Invalid,
-		GreaterThan,
-		GreaterThanOrEqual,
-		Equals,
-		BackSlash,
-		Period,
-		EndOfFile
+
+
+
+	enum LexemeType{ 
+		/* Invalid */
+
+		 MP_INVALID ,
+
+		/* Identifiers and Literals */
+
+		MP_IDENTIFIER , //(letter | "_"(letter | digit)){["_"](letter | digit)}
+		MP_INTEGER_LIT , //digit{digit}
+		MP_FIXED_LIT , //digit{digit} "." digit{digit} 
+		MP_FLOAT_LIT , // (digit{digit} | digit{digit} "." digit{digit}) ("e"|"E")["+"|"-"]digit{digit}
+		MP_STRING_LIT , //"'" {"''" | AnyCharacterExceptApostropheOrEOL} "'" 
+
+		/* Symbols */
+
+		MP_PERIOD, //         "."
+		MP_COMMA , //         ","
+		MP_SCOLON, //         ";"
+		MP_LPAREN, //         "("
+		MP_RPAREN, //        ")"
+		MP_EQUAL,  //        "="
+		MP_GTHAN,  //        ">"
+		MP_GEQUAL, //        ">="
+		MP_LTHAN,  //        "<"
+		MP_LEQUAL, //         "<="
+		MP_NEQUAL, //         "<>"
+		MP_ASSIGN, //        ":="
+		MP_PLUS, //          "+"
+		MP_MINUS, //         "-"
+		MP_TIMES, //         "*"
+		MP_COLON, //		":"
+
+		/* End of File */
+
+		MP_EOF,
+
+		/* Errors */
+
+		MP_RUN_COMMENT, // starts with { and end with a }
+		MP_RUN_STRING,
+		MP_ERROR,
+
 	};
     
 
-	string TypeNames[0] = { "Invalid"
-			"GreaterThan"
-			"GreaterThanOrEqual"
-			"Equals"
-			"BackSlash"
-			"Period"
-			"EndOfFile"
+	char const* TypeAsString [26] =  
+	{ 
+		/* Invalid */
+
+		"MP_INVALID",
+
+		/* Identifiers and Literals */
+
+		"MP_IDENTIFIER", //(letter | "_"(letter | digit)){["_"](letter | digit)}
+		"MP_INTEGER_LIT", //digit{digit}
+		"MP_FIXED_LIT", //digit{digit} "." digit{digit} 
+		"MP_FLOAT_LIT", // (digit{digit} | digit{digit} "." digit{digit}) ("e"|"E")["+"|"-"]digit{digit}
+		"MP_STRING_LIT", //"'" {"''" | AnyCharacterExceptApostropheOrEOL} "'" 
+
+		/* Symbols */
+
+		"MP_PERIOD", //         "."
+		"MP_COMMA", //         ","
+		"MP_SCOLON", //         ";"
+		"MP_LPAREN", //         "("
+		"MP_RPAREN", //        ")"
+		"MP_EQUAL",  //        "="
+		"MP_GTHAN",  //        ">"
+		"MP_GEQUAL", //        ">="
+		"MP_LTHAN",  //        "<"
+		"MP_LEQUAL", //         "<="
+		"MP_NEQUAL", //         "<>"
+		"MP_ASSIGN", //        ":="
+		"MP_PLUS", //          "+"
+		"MP_MINUS", //         "-"
+		"MP_TIMES", //         "*"
+		"MP_COLON", //		":"
+
+		/* End of File */
+
+		"MP_EOF",
+
+		/* Errors */
+
+		"MP_RUN_COMMENT", // starts with { and end with a }
+		"MP_RUN_STRING",
+		"MP_ERROR",
 	};
 
 	static string typeToName(Type type);
