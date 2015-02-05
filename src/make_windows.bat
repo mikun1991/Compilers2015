914d37c15b65d825647e0d642be50e607a47a280
BickfordA <aidan.bickford@gmail.com>
2015-02-04 19:12:04 -0700

    Added more test cases

--- src/make_windows.bat
+++ src/make_windows.bat
@@ -2,4 +2,4 @@ rm -rf _build
 mkdir _build
 cd _build
 
-cmake .. -G"Visual Studio 12 2013"
+cmake .. -G"Visual Studio 12 2013" -DCMAKE_BUILD_TYPE=Debug
--- src/scanner/TestScanner/TestScanner.cpp
+++ src/scanner/TestScanner/TestScanner.cpp
@@ -28,18 +28,82 @@ bool TestScanner::testNumber()
 		int col = 0;
 		Token ret = FiniteStateAutomaton::number(&ss, line, col);
 		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_INTEGER_LIT);
+		assert(line == 0);
+		assert(col == 6);
 	}
 
-	//TestCase 1
+	//TestCase 2
 	{
 		std::stringstream ss;
 
-		ss << "3E4";
+		ss << "3E+4";
 
 		int line = 0;
 		int col = 0;
 		Token ret = FiniteStateAutomaton::number(&ss, line, col);
-		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_INTEGER_LIT);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_FLOAT_LIT);
+		assert(line == 0);
+		assert(col == 4);
+	}
+
+	//TestCase 3
+	{
+		std::stringstream ss;
+
+		ss << "3E-4   dfs";
+
+		int line = 0;
+		int col = 0;
+		Token ret = FiniteStateAutomaton::number(&ss, line, col);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_FLOAT_LIT);
+	}
+
+	//TestCase 4
+	{
+		std::stringstream ss;
+
+		ss << "3.893245E+4   Next";
+
+		int line = 0;
+		int col = 0;
+		Token ret = FiniteStateAutomaton::number(&ss, line, col);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_FIXED_LIT);
+	}
+
+	//TestCase 5
+	{
+		std::stringstream ss;
+
+		ss << "3.893245E+4   Next";
+
+		int line = 0;
+		int col = 0;
+		Token ret = FiniteStateAutomaton::number(&ss, line, col);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_FIXED_LIT);
+	}
+
+	//TestCase 6
+	{
+		std::stringstream ss;
+
+		ss << " 3.893245E+4   Next";
+
+		int line = 0;
+		int col = 0;
+		Token ret = FiniteStateAutomaton::number(&ss, line, col);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_INVALID);
+	}
+
+	//TestCase 6
+	{
+		std::stringstream ss;
+
+		ss << "dog3.893245E+4   Next";
+
+		int line = 0;
+		int col = 0;
+		Token ret = FiniteStateAutomaton::number(&ss, line, col);
+		assert(ret.getLexeme().getType() == Lexeme::LexemeType::MP_INVALID);
 	}
 
 	return true;
